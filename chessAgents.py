74c71c91b4b75ce0d7e759f64599c0bc6240ea64
Vladlen <ilusenkovladlen@gmail.com>
2022-10-24 18:41:36 +0300

    feat: added materialScore to evaluationFunction

--- chessAgents.py
+++ chessAgents.py
@@ -1,26 +1,61 @@
 import chess
 
 
-class Agent:
+class ChessAgent:
     @staticmethod
     def evaluationFunction(gameState):
-        return 1
+        # Figures num
+        wp = len(gameState.pieces(chess.PAWN, chess.WHITE))
+        bp = len(gameState.pieces(chess.PAWN, chess.BLACK))
+        wn = len(gameState.pieces(chess.KNIGHT, chess.WHITE))
+        bn = len(gameState.pieces(chess.KNIGHT, chess.BLACK))
+        wb = len(gameState.pieces(chess.BISHOP, chess.WHITE))
+        bb = len(gameState.pieces(chess.BISHOP, chess.BLACK))
+        wr = len(gameState.pieces(chess.ROOK, chess.WHITE))
+        br = len(gameState.pieces(chess.ROOK, chess.BLACK))
+        wq = len(gameState.pieces(chess.QUEEN, chess.WHITE))
+        bq = len(gameState.pieces(chess.QUEEN, chess.BLACK))
+        wk = len(gameState.pieces(chess.KING, chess.WHITE))
+        bk = len(gameState.pieces(chess.KING, chess.BLACK))
+
+        # Figures importance
+        p_weight = 10
+        n_weight = 100
+        b_weight = 300
+        r_weight = 500
+        q_weight = 1000
+        k_weight = 30000
+
+        # The more important figures - the better position is
+        material_score = p_weight * (wp - bp) + n_weight * (wn - bn) + b_weight * (wb - bb) + r_weight * (wr - br) \
+                         + q_weight * (wq - bq) + k_weight * (wk - bk)
+
+        return material_score
 
     def __init__(self, index=0, depth=2):
         self.index = int(index)
         self.depth = int(depth)
 
 
-class NegamaxAgent(Agent):
+class NegamaxChessAgent(ChessAgent):
     def getAction(self):
         pass
 
-
-class NegascoutAgent(Agent):
-    def getAction(self):
+    def negamax(self):
         pass
 
 
-class PvsAgent(Agent):
+class NegascoutChessAgent(ChessAgent):
     def getAction(self):
+        pass
+
+    def negascout(self):
+        pass
+
+
+class PvsChessAgent(ChessAgent):
+    def getAction(self):
+        pass
+
+    def pvs(self):
         pass
